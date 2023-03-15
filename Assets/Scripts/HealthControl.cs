ae9d326f7a71ac35b7f1e697228d91f33a9d9261
EdHall <halluchroger@gmail.com>
2023-03-15 14:37:29 -0300

    Add BarHealth

---
+++ Assets/Scripts/HealthControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthControl : MonoBehaviour
+{
+    public float health = 2;
+    public float Health { get {return health;} set{health = Mathf.Clamp(value,0,healthMax);} }
+    public float healthMax = 14;
+
+    public Image healthBar;
+
+    private  void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = Health / healthMax;
+    }
+}
