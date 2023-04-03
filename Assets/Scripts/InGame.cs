997ce1c1a39fae2f3f7e985154bba3b2cb65e21e
EdHall <halluchroger@gmail.com>
2023-04-03 14:28:06 -0300

    IU da pontuacao

---
+++ Assets/Scripts/InGame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGame : MonoBehaviour
+{
+
+    public Text textPoint;
+    // Update is called once per frame
+    void Update()
+    {
+        this.textPoint.text = PointController.Point.ToString();
+    }
+}
